498645913064a1b9bd0310b601751df6bcff1cd4
fifymawia <mawiafreda@gmail.com>
2019-10-21 11:12:59 +0000

    User Authentication when deleting,editing and checking added BOOM!

--- imports/api/eits.js
+++ imports/api/eits.js
@@ -36,6 +36,12 @@ Meteor.methods({
         if (! this.userId) {
           throw new Meteor.Error('not-authorized');
         }
+
+        const eit = Eits.findOne({ _id: id });
+
+        if (Meteor.user().username !== eit.username) {
+            throw new Meteor.Error('not-authorized');
+          }
      
         Eits.update(id, {
         $set: {
@@ -48,18 +54,35 @@ Meteor.methods({
     },
     'eits.remove'(eitId) {
       check(eitId, String);
+
+      const eit = Eits.findOne({ _id: eitId });
+
+      if (Meteor.user().username !== eit.username) {
+        throw new Meteor.Error('not-authorized');
+      }
    
       Eits.remove(eitId);
     },
     'eits.remove_bulk'() {
         const eits = Eits.find({ checked: true });
         eits.forEach(eit => {
+            if (Meteor.user().username !== eit.username) {
+                throw new Meteor.Error('not-authorized');
+              }
+
             Eits.remove(eit._id);
         });
     },
     'eits.setChecked'(eitId, setChecked) {
       check(eitId, String);
       check(setChecked, Boolean);
+
+      const eit = Eits.findOne(eitId);
+      //console.log(eit);
+
+      if (Meteor.user().username !== eit.username) {
+        throw new Meteor.Error('not-authorized');
+      }
    
       Eits.update(eitId, { $set: { checked: setChecked } });
     },
