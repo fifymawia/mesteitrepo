2ffc2653b69d7d22b40c3a239ab55811159213b5
fifymawia <mawiafreda@gmail.com>
2019-10-22 15:50:27 +0000

    Routing added on About

--- imports/ui/about.js
+++ imports/ui/about.js
@@ -4,8 +4,13 @@ import React, { Component } from 'react';
 export default class About extends Component {
     render(){
         return(
+            <div>
 
             <h1>About EIT managment System</h1> 
+
+            <p> Eit managment System Is Dayos assignment and am done!!! . my UI sucks Though, i will hire someone</p>
+
+            </div>
  
         )
     }
